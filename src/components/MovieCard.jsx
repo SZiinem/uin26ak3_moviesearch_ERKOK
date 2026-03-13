8da03ff5c86d74159719ec89acffba59a83a8d08
SZiinem <sineme@hiof.no>
2026-03-13 15:41:59 +0100

    La til slug for router og hentet ut informasjon for enkelte filmer

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,17 @@
 import '../style/moviecard.scss'
+import { Link } from 'react-router-dom'
 
 export default function MovieCard({movie}){
+
+    const slug = movie.Title
+
     return(
         <article key={movie.imdbID} className="movie-card">
-            <img src={movie.Poster} alt={movie.Title}/>
-            <h3>{movie.Title}</h3>
-            <p>{movie.Year}</p>
+            <Link to={`/${slug}`}>
+                <img src={movie.Poster} alt={movie.Title}/>
+                <h3>{movie.Title}</h3>
+                <p>{movie.Year}</p>
+            </Link>
         </article>
     )
 }
--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,10 +1,37 @@
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
 export default function Movie(){
 
     const{movie} = useParams()
+    const [movieInfo, setMovieInfo] = useState()
+    const apiKey = import.meta.env.VITE_APP_API_KEY
+
+    const getMovie = async(Title)=>{
+
+        const response = await fetch(`https://www.omdbapi.com/?t=${encodeURIComponent(Title)}&apikey=${apiKey}`)
+        const data = await response.json()
+        setMovieInfo(data)
+        console.log(data)
+    }
+
+    useEffect(()=>{
+        getMovie(movie)
+    }, [movie])
 
     return(
-        <h1>{movie}</h1>
+        <section className="movie-details">
+            <h1>{movieInfo?.Title}</h1>
+            <p><strong>Utgitt: </strong>{movieInfo?.Year}</p>
+            <img src={movieInfo?.Poster} alt={movieInfo?.Title}/>
+
+            <article className="extra-info">
+                <p>{movieInfo?.Plot}</p>
+                <p><strong>Sjanger: </strong>{movieInfo?.Genre}</p>
+                <p><strong>Direktør: </strong>{movieInfo?.Director}</p>
+                <p><strong>Skuespillere: </strong>{movieInfo?.Actors}</p>
+            </article>
+
+        </section>
     )
 }
